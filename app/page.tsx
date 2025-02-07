46b599c48777420199d2d794aec179cd2a5f3358
eddysfc <159918813+eddysfc@users.noreply.github.com>
2025-02-07 15:15:24 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,23 +23,23 @@ export default function Page() {
 
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          Hi! My name is Jeffrey Lu. I am pursuing a B.S. in Computer Science at 
-          the University of Illinois Urbana-Champaign. My interests lie at the 
-          intersection of computer science and innovation, and I am passionate 
+          Hi! My name is Joe Mama. I am pursuing a B.S. in Computer Science at
+          the University of Illinois Urbana-Champaign. My interests lie at the
+          intersection of computer science and innovation, and I am passionate
           about leveraging technology to solve real-world problems.
         </p>
         <p>
-          During my academic journey, I’ve worked on diverse projects ranging from 
-          software development to data-driven research, exploring fields such as AI, 
-          machine learning, and simulations. I enjoy diving deep into complex challenges, 
-          whether it’s through hands-on coding, algorithm optimization, or uncovering 
+          During my academic journey, I’ve worked on diverse projects ranging from
+          software development to data-driven research, exploring fields such as AI,
+          machine learning, and simulations. I enjoy diving deep into complex challenges,
+          whether it’s through hands-on coding, algorithm optimization, or uncovering
           insights from data.
         </p>
         <p>
-          Beyond academics, I’ve had the privilege of mentoring students, engaging in 
-          community outreach, and sharing my passion for learning through programs I’ve 
-          developed. When I’m not coding, you’ll often find me immersed in strategy or 
-          rhythm games, stargazing to explore my love for astronomy, reading about 
+          Beyond academics, I’ve had the privilege of mentoring students, engaging in
+          community outreach, and sharing my passion for learning through programs I’ve
+          developed. When I’m not coding, you’ll often find me immersed in strategy or
+          rhythm games, stargazing to explore my love for astronomy, reading about
           emerging technologies, or discovering new ways to challenge myself.
         </p>
         <p>
